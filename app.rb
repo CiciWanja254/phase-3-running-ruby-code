8a75e068182a01b468a4a23c3c563391b845aea8
Cici254 <thukucindy00@gmail.com>
2023-05-16 22:32:45 +0300

    Complete assignment

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
+# pp [{ id: 1, hello: "World" },
+#     { id: 2, hello: "Ruby" },
+#     { id: 3, hello: "Moon" },
 #     { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
@@ -14,10 +14,13 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
+def add(num1, num2)
     puts num1
     puts num2
     num1 + num2
 end
 
-puts add(1,3) + add(2,2)
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
+puts add(1, 3) + add(2, 2)
